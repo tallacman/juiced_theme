1ffea87d7bbbe0320fb2f3914ad1be65aa3b724e
tallacman <stev@mac.com>
2015-12-25 15:50:25 -0800

    changes

--- themes/juiced/elements/page_header.php
+++ themes/juiced/elements/page_header.php
@@ -4,16 +4,21 @@
 <!--   page header code required by html and concrete5    -->
 
 <html class="no-js" lang="<?php echo Localization::activeLanguage()?>">
+<!--  no jus used by modernizr :: language defined by c5 settings     -->
+
 <head>
 <?php  Loader::element('header_required'); ?>
+<!--  first element after head tag     -->
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--  sets the page for media calls for responsiveness     -->
 <link rel="stylesheet" href=https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css>
 
 <?php echo $html->css($view->getStylesheet('main.less'))?>
+<!--   less file concatinates all the styles; compressible by c5     -->
 
-<!--  this gets moved to the main.less file on publishing     -->
 <link rel="stylesheet" href="<?php echo $this->getThemePath()?>css/theme.css" />
-
+<!--  this gets moved to the main.less file on publishing     -->
 
 <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
 
